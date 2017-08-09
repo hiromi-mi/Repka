f6fba1eec341c458183f4ccd9a81701fefd9ebec
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2017-08-09 23:50:08 +0900

    fix root routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root to: redirect('/login')
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
