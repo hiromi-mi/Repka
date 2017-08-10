0ca307328e4fe5f1b8fb8d873e8b7759599585e2
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2017-08-10 12:48:57 +0900

    add url validation

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,4 +2,5 @@ class Piece < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :teacher, length: { maximum: 255 }
   validates :kind, length: { maximum: 255 }
+  validates :data, format: /\A#{URI::regexp(%w(http https))}\z/
 end
