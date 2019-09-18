7f71c45d2af1006405b83c29e6c680e6c4645a39
hiromi-mi <hiromi-mi@cat.zaq.jp>
2019-09-19 01:29:59 +0900

    email/password -> name/password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to pieces_path
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid name/password combination'
       render 'new'
     end
   end
